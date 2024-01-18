a38bb1d8e6bc64db782b56824c1c8097b40d8364
rupertJones05 <owensito2005@gmail.com>
2024-01-18 13:36:45 -0800

    hi

---
+++ core/src/com/bmhs/gdxintro/gfx/utils/TileHandler.java
@@ -0,0 +1,66 @@
+package com.bmhs.gdxintro.gfx.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.bmhs.gdxintro.gfx.assets.Tile;
+import com.bmhs.gdxintro.gfx.assets.WorldTile;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+public class TileHandler {
+
+    public static TileHandler tileHandler = null;
+    private int spriteOnSheetHeight, spriteOnSheetWidth;
+    private String primaryColorSheetPath = "primaryColorSheet.png";
+    private Array<WorldTile> worldTileArray;
+    private TileHandler() {
+        worldTileArray = new Array<>();
+
+        spriteOnSheetHeight = Tile.ON_SCREEN_DEFAULT_HEIGHT;
+        spriteOnSheetWidth = Tile.ON_SCREEN_DEFAULT_WIDTH;
+
+        Texture tempTexture = new Texture(primaryColorSheetPath);
+        TextureRegion tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*2, spriteOnSheetHeight*0, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile blackWorldTile = new WorldTile(tempRegion, 0, "black");
+        worldTileArray.add(blackWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*3, spriteOnSheetHeight*0, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile whiteWorldTile = new WorldTile(tempRegion, 1, "white");
+        worldTileArray.add(whiteWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*2, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile redWorldTile = new WorldTile(tempRegion, 2, "red");
+        worldTileArray.add(redWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*3, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile orangeWorldTile = new WorldTile(tempRegion, 3, "orange");
+        worldTileArray.add(orangeWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*4, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile yellowWorldTile = new WorldTile(tempRegion, 4, "green");
+        worldTileArray.add(yellowWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*5, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile greenWorldTile = new WorldTile(tempRegion, 5, "green");
+        worldTileArray.add(greenWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*6, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile oddGreenWorldTile = new WorldTile(tempRegion, 6, "oddGreen");
+        worldTileArray.add(oddGreenWorldTile);
+
+        tempRegion = new TextureRegion(tempTexture, spriteOnSheetWidth*0, spriteOnSheetHeight*7, spriteOnSheetWidth, spriteOnSheetHeight);
+        WorldTile blueWorldTile = new WorldTile(tempRegion, 5, "blue");
+        worldTileArray.add(blueWorldTile);
+
+
+    }
+
+    public Array<WorldTile> getWorldTileArray() {
+        return worldTileArray;
+    }
+    public static TileHandler getTileHandler(){
+            if(tileHandler == null) {
+                tileHandler = new TileHandler();
+            }
+            return tileHandler;
+        }
+    }
+
